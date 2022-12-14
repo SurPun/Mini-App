5ad6288e880a316f3eff79e61ab40559ca77b45a
Suraj Pun <s_pun@outlook.com>
2022-12-14 23:23:47 +0000

    Features & Styles

--- README.md
+++ README.md
@@ -12,20 +12,20 @@ Your mini-app should take user input for
 
 When the user clicks ‘Next’, it should display the recently added
 
-- [ ] category name,
-- [ ] icon
-- [ ] colour, with a ‘Back’ button (not designed).
+- [x] category name,
+- [x] icon
+- [x] colour, with a ‘Back’ button (not designed).
 
 When the user clicks ‘Back’,
 
-- [ ] the newly created category should now be added to the list and displayed.
+- [x] the newly created category should now be added to the list and displayed.
 
 When the user chooses a previously made category and clicks ‘next’,
 
-- [ ] it should display the previously set category name, icon and colour.
+- [x] it should display the previously set category name, icon and colour.
 
 The user is also able to
 
-- [ ] delete the category by hovering over the category and pressing the ‘delete’ (rubbish bin) icon.
+- [x] delete the category by hovering over the category and pressing the ‘delete’ (rubbish bin) icon.
 
-- [ ] Use localStorage or sessionStorage to store the data
+- [x] Use localStorage or sessionStorage to store the data
--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 // Displays Logo Path
 document.querySelector("#logo").addEventListener("change", function () {
   // Regex to format Fig Path
-  const logoName = this.value.replace(/^.*\\/, "");
+  var logoName = this.value.replace(/^.*\\/, "");
 
   // Display FileName
-  document.getElementById("display-logo").innerHTML = logoName;
+  document.getElementById("display-logo-name").innerHTML = logoName;
 
   // Store to Storage
   const reader = new FileReader();
 
   reader.addEventListener("load", () => {
-    localStorage.setItem("recent-logo", reader.result);
+    sessionStorage.setItem("recent-logo", reader.result);
+    sessionStorage.setItem("logo-name", logoName);
   });
 
   reader.readAsDataURL(this.files[0]);
 });
 
-// function getColour() {
-//   console.log(document.getElementById("colour-select").value);
-// }
+function clearStorage() {
+  sessionStorage.clear();
+  window.location.reload();
+}
+
+function saveData() {
+  const category = document.getElementById("category-name");
+  const colour = document.getElementById("colour-select");
+
+  sessionStorage.setItem("category", category.value);
+  sessionStorage.setItem("colour", colour.value);
+}
+
+// Display List
+const displayList = document.querySelector(".list-container");
+
+for (let i = 0; i < 20; i++) {
+  const logoName = sessionStorage.getItem("logo-name");
+  const categoryData = sessionStorage.getItem("category");
+  const colourData = sessionStorage.getItem("colour");
+
+  if (categoryData) {
+    displayList.innerHTML = `
+    <h2><a href="/results.html?category=${categoryData}&logo=${logoName}&colour=${colourData}">${categoryData}</a></h2>
+    <button class="delBtn" onclick="clearStorage()">Delete</button>
+    <span></span>
+    `;
+  }
+}
